fca2a55367ea555b53552db2ace3c87603d85d8c
Filipe Freire <filipefreire@protonmail.com>
2020-08-17 11:22:42 +0100

    Complete lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import countries from './countries.json';
 import Navbar from './components/Navbar';
@@ -6,29 +6,58 @@ import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
 import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-        <Navbar title="WikiCountries" />
-      </div>
-      <div className="container">
-        <div className="row">
-          <div
-            className="col-5"
-            style={{ maxHeight: '90vh', overflow: 'scroll' }}
-          >
-            <div className="list-group">
-              <CountriesList countries={countries} />
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://countries.tech-savvy.tech/countries')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            countries: result,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <Navbar title="WikiCountries" />
+          {console.log(this.state.countries)}
+        </div>
+        <div className="container">
+          <div className="row">
+            <div
+              className="col-5"
+              style={{ maxHeight: '90vh', overflow: 'scroll' }}
+            >
+              <div className="list-group">
+                <CountriesList countries={countries} />
+              </div>
             </div>
+            <Switch>
+              <Route path="/:id" component={CountryDetails} />
+            </Switch>
           </div>
-          <Switch>
-            <Route path="/:id" component={CountryDetails} />
-          </Switch>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
